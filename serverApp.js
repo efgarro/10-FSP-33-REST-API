5e23c0713cfe8be3bcd27878ac1b778ed80dbbaf
efgarro <efgarro@yahoo.com>
2022-11-15 14:45:12 -0600

    2022-11-15e

--- serverApp.js
+++ serverApp.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const serverApp = express();
 // const cors = require("cors");
-const apiRouter = require("./srcServer/Routes/apiRouter");
+// const apiRouter = require("./srcServer/Routes/apiRouter");
 
 /* Do not change the following line! It is required for testing and allowing
  *  the frontend application to interact as planned with the api server
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 4000;
 // Add middleware for handling CORS requests from index.html
 // serverApp.use(cors());
 // Add middware for parsing request bodies here:
-serverApp.use(express.json());
-serverApp.use(express.urlencoded({ extended: false }));
+// serverApp.use(express.json());
+// serverApp.use(express.urlencoded({ extended: false }));
 
 // serverApp.use((req, res, next) => {
 //     res.header("Access-Control-Allow-Origin", "*");
@@ -27,7 +27,7 @@ serverApp.use(express.urlencoded({ extended: false }));
 
 // Mount your existing apiRouter below at the '/api' path.
 serverApp.get("/", (req, res) => res.send("Hellow Bella!"));
-serverApp.use("/api", apiRouter);
+// serverApp.use("/api", apiRouter);
 
 // This conditional is here for testing purposes:
 if (!module.parent) {
