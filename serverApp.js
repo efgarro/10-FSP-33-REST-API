67638cb0fa274329fdef8e654dbf001e32b1ecca
efgarro <efgarro@yahoo.com>
2022-11-14 20:50:36 -0600

    access middleware

--- serverApp.js
+++ serverApp.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const serverApp = express();
 const cors = require("cors");
-module.exports = serverApp;
+const apiRouter = require("./srcServer/Routes/apiRouter");
 
 /* Do not change the following line! It is required for testing and allowing
- *  the frontend application to interact as planned with the api server
- */
+*  the frontend application to interact as planned with the api server
+*/
 const PORT = process.env.PORT || 4000;
 
 // Add middleware for handling CORS requests from index.html
@@ -14,15 +14,27 @@ serverApp.use(cors());
 serverApp.use(express.json());
 serverApp.use(express.urlencoded({ extended: false }));
 
-// Mount your existing apiRouter below at the '/api' path.
-const apiRouter = require("./srcServer/Routes/apiRouter");
-serverApp.use("/api", apiRouter);
-
-// This conditional is here for testing purposes:
-if (!module.parent) {
-  // Add your code to start the server listening at PORT below:
-
-  serverApp.listen(PORT, () => {
-    console.log(`Listening to port...${PORT}`);
-  });
-}
+serverApp.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+        next();
+    });
+    
+    // Mount your existing apiRouter below at the '/api' path.
+    serverApp.use("/api", apiRouter);
+    
+    // This conditional is here for testing purposes:
+    if (!module.parent) {
+        // Add your code to start the server listening at PORT below:
+        
+        serverApp.listen(PORT, () => {
+            console.log(`Listening to port...${PORT}`);
+        });
+    }
+    
+    module.exports = serverApp;
