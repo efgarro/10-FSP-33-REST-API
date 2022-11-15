67781c5fdddb36ee6989c59d3199201d30f2d35f
efgarro <efgarro@yahoo.com>
2022-11-14 22:09:05 -0600

    Hellow Bella

--- serverApp.js
+++ serverApp.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const serverApp = express();
-const cors = require("cors");
-const apiRouter = require("./srcServer/Routes/apiRouter");
+// const cors = require("cors");
+// const apiRouter = require("./srcServer/Routes/apiRouter");
 
 /* Do not change the following line! It is required for testing and allowing
 *  the frontend application to interact as planned with the api server
@@ -9,24 +9,24 @@ const apiRouter = require("./srcServer/Routes/apiRouter");
 const PORT = process.env.PORT || 4000;
 
 // Add middleware for handling CORS requests from index.html
-serverApp.use(cors());
+// serverApp.use(cors());
 // Add middware for parsing request bodies here:
-serverApp.use(express.json());
-serverApp.use(express.urlencoded({ extended: false }));
+// serverApp.use(express.json());
+// serverApp.use(express.urlencoded({ extended: false }));
 
-serverApp.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-        );
-        res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-        next();
-    });
+// serverApp.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept"
+//         );
+//         res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//         next();
+//     });
     
     // Mount your existing apiRouter below at the '/api' path.
-    serverApp.use("/api", apiRouter);
+    serverApp.use("/", (req, res) => res.send('Hellow Bella!!'));
     
     // This conditional is here for testing purposes:
     if (!module.parent) {
