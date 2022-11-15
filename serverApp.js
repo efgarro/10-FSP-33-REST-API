1438f5ded01886c62ac8c5f5ba3f2f3dc2438b46
efgarro <efgarro@yahoo.com>
2022-11-15 14:30:41 -0600

    2022-11-15c

--- serverApp.js
+++ serverApp.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const serverApp = express();
-const cors = require("cors");
+// const cors = require("cors");
 const apiRouter = require("./srcServer/Routes/apiRouter");
 
 /* Do not change the following line! It is required for testing and allowing
-*  the frontend application to interact as planned with the api server
-*/
+ *  the frontend application to interact as planned with the api server
+ */
 const PORT = process.env.PORT || 4000;
 
 // Add middleware for handling CORS requests from index.html
-serverApp.use(cors());
+// serverApp.use(cors());
 // Add middware for parsing request bodies here:
 serverApp.use(express.json());
 serverApp.use(express.urlencoded({ extended: false }));
@@ -24,17 +24,18 @@ serverApp.use(express.urlencoded({ extended: false }));
 //         res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
 //         next();
 //     });
-    
-    // Mount your existing apiRouter below at the '/api' path.
+
+// Mount your existing apiRouter below at the '/api' path.
+serverApp.get("/", (req, res) => res.send("Hellow Bella!"));
 serverApp.use("/api", apiRouter);
-    
-    // This conditional is here for testing purposes:
-    if (!module.parent) {
-        // Add your code to start the server listening at PORT below:
-        
-        serverApp.listen(PORT, () => {
-            console.log(`Listening to port...${PORT}`);
-        });
-    }
-    
-    module.exports = serverApp;
+
+// This conditional is here for testing purposes:
+if (!module.parent) {
+  // Add your code to start the server listening at PORT below:
+
+  serverApp.listen(PORT, () => {
+    console.log(`Listening to port...${PORT}`);
+  });
+}
+
+module.exports = serverApp;
