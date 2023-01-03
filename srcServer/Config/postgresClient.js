cfe1dc173054c0baeaa18755da4b06df45f603b9
efgarro <efgarro@yahoo.com>
2023-01-02 21:53:36 -0600

    updated az-db

--- srcServer/Config/postgresClient.js
+++ srcServer/Config/postgresClient.js
@@ -1,35 +1,35 @@
-// const { Client } = require("pg");
-// require("dotenv").config();
-
-// const pgClientPassword = process.env.PG_PWD;
-
-// const pgClient = new Client({
-//   host: "soy-crc-db-server.postgres.database.azure.com",
-//   port: 5432,
-//   database: "scr-db-v2022b",
-//   user: "efgarro",
-//   password: pgClientPassword,
-//   ssl: true,
-// });
-
-// pgClient.connect();
-
-// module.exports = { pgClient };
-
-
 const { Client } = require("pg");
 require("dotenv").config();
 
-const pgClientPassword = process.env.PG_PWD_LH;
+const pgClientPassword = process.env.PG_PWD;
 
 const pgClient = new Client({
-  host: "localhost",
+  host: "soy-crc-db-server.postgres.database.azure.com",
   port: 5432,
-  database: "scr-db-v2022a",
+  database: "scr-db-v2022b",
   user: "efgarro",
   password: pgClientPassword,
+  ssl: true,
 });
 
 pgClient.connect();
 
 module.exports = { pgClient };
+
+
+// const { Client } = require("pg");
+// require("dotenv").config();
+
+// const pgClientPassword = process.env.PG_PWD_LH;
+
+// const pgClient = new Client({
+//   host: "localhost",
+//   port: 5432,
+//   database: "scr-db-v2022a",
+//   user: "efgarro",
+//   password: pgClientPassword,
+// });
+
+// pgClient.connect();
+
+// module.exports = { pgClient };
